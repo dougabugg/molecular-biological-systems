1aed0dbcd4d9aeaa7a740ab07f23cd081ca22ba8
Doug <drezabek@protonmail.com>
2022-01-28 14:04:35 -0600

    no code just some notes

---
+++ libraries/mbs/src/lib.rs
@@ -0,0 +1,45 @@
+// this will just be some random brain storming for now
+
+/*
+Articles on Wikipedia of interest:
+https://en.wikipedia.org/wiki/Jmol
+https://en.wikipedia.org/wiki/Chemical_file_format
+https://en.wikipedia.org/wiki/GROMACS
+https://en.wikipedia.org/wiki/Chemical_nomenclature
+https://en.wikipedia.org/wiki/Preferred_IUPAC_name
+*/
+
+/*
+This is such a massive "rabbit hole", it feels very overwhelming. When I initially had the idea of creating this
+repository, I just wanted to make a database, where you could look up a molecule, like "serotonin", and find which
+cells in the human body it can interact with, and what results from an interaction. Then I get stuck into reading
+about the GROMACS project, and molecular dynamics, but that's not what I want this project to be. I know other
+software exists for doing simulations, and they've been optimized by more people and for much longer than I can
+ever accomplish on my own. So what exactly is my goal in making this project? I want to try and give "structure"
+and "meaning" to all this arbitrary data. Yes, Serotonin has the chemical formula "C(10) H(12) N(2) O", and a myriad
+of other properties, but how does it fit into a biological system as a whole? What occurs inside of a cell when
+serotonin binds to it's receptors, and what is the result on the local tissues and the host/macro organism?
+
+When I go into wikipedia, and read the article about serotonin, there's this table with the name "Binding profile
+of serotonin", which lists all these "meaninglessly" named receptors, like "5-HT1A" or "5-HT2B", and even more
+"meaningless" statistics and units for "inhibition constant (Ki)" or "dissociation constant (Kd)" in "nanomolars
+(nM)". I will admit, there's a lack and disconnect in my understanding of biology and chemistry; but I don't need
+to deeply understand "organic chemistry" to understand certain higher-level concepts about biology, like "cell
+respiration", or "DNA/RNA transcription". That is the goal of this project, to try and link together these high-
+level concepts, and encode the most general rules and relationships in some sort of data base or data structure,
+that then let's my curious mind ask questions like "what is actually going on when X Y and Z happen?", and sift
+thru this gigantic model and attempt to come up with some "theoretical explanation".
+
+It's so difficult for me to start building something this abstract; I find myself wanting to build something like
+GROMACS, but that's not a productive use of my time, and doesn't help me answer these high-level conceptual
+questions. Really, this thing I'm trying to build is sort of an information organization framework, with the
+ability to create and follow links between items and nodes, and make "queries". It's such an abstract definition,
+I don't really know how to begin coding!
+*/
+
+/*
+I didn't end up writing any code for this project today. But I'll at least push the notes I've taken on the topic
+so far. In the event that I think about this again in the future, and want to build some sort of "tool" to help
+me navigate the massive complexity of molecular biology, maybe I'll find these notes again, and it will help me
+start writing some code.
+*/
